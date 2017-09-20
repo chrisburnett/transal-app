85c3d0f11d38c8fe547a64f2562a7d44fb81a6e3
Chris Burnett <dameramu@gmail.com>
2017-09-20 03:15:43 +0200

    Endpoint URL in config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,5 +9,6 @@ export interface IAppConfig {
 
 export const AppConfig: IAppConfig = {
 	apiEndpoint: "localhost:3000/api/v1",
+	//apiEndpoint: "hidden-caverns-63006.herokuapp.com/api/v1",
 	lang: 'pl'
 }
