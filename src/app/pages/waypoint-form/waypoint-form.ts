91222bd3ffc57b629397ba413175daf552ed840e
Chris Burnett <dameramu@gmail.com>
2017-09-22 00:57:03 +0200

    Fuel data and milage in waypoints

--- src/app/pages/waypoint-form/waypoint-form.ts
+++ src/app/pages/waypoint-form/waypoint-form.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Waypoint } from '../../waypoint';
+import { Reading } from '../../reading';
 import { WaypointService } from '../../../providers/waypoint-service/waypoint-service';
 import { Assignment } from '../../assignment';
 
@@ -25,25 +26,33 @@ export class WaypointFormPage implements OnInit {
 			reading_attributes: this.formBuilder.group({
 				odometer: [''],
 				maut: ['']
-			})
+			}),
+			additional_km: [''],
+			price_per_litre: [''],
+			litres_diesel: ['']
 		});
 	}
 
 	ngOnInit(): void {
-		this.waypoint = new Waypoint();
+		this.waypoint = this.navParams.get("waypoint");
+
+		// if no waypoint passed then we are creating unscheduled
+		// point - set up default values
+		if(this.waypoint == null)
+		{
+			let currentAssignment: Assignment = this.navParams.get("currentAssignment");
+			this.waypoint = new Waypoint();
+		 	this.waypoint.scheduled = false;
+			this.waypoint.route_id = currentAssignment.route.id;
+			this.waypoint.reading_attributes = new Reading();
+			this.waypoint.reading_attributes.truck_id = currentAssignment.truck_id;
+			this.waypoint.scheduled_date = new Date();
+			this.waypoint.actual_date = this.waypoint.scheduled_date;
+		}
 	}
 
 	submit(): void {
-		let currentAssignment: Assignment = this.navParams.get("currentAssignment");
 		this.waypoint = this.waypointForm.value;
-		this.waypoint.route_id = currentAssignment.route.id;
-		this.waypoint.scheduled = false;
-		this.waypoint.reading_attributes.truck_id = currentAssignment.truck_id;
-		this.waypoint.scheduled_date = new Date();
-		this.waypoint.actual_date = this.waypoint.scheduled_date;
-
-		//TODO: THIS IS WHERE CALL WILL START TO POST NEW WP, CHECK THIS WORKS
 		this.waypointService.create(this.waypoint).subscribe(() => this.navCtrl.pop());
 	}
-	
 }
