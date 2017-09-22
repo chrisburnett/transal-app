f32e74ebffba1756f6f8fd78366c0315a2a8484d
Chris Burnett <dameramu@gmail.com>
2017-09-22 03:10:18 +0200

    New waypoint validations

--- src/app/pages/waypoint-form/waypoint-form.ts
+++ src/app/pages/waypoint-form/waypoint-form.ts
@@ -15,10 +15,11 @@ export class WaypointFormPage implements OnInit {
 
 	waypoint: Waypoint;
 	waypointForm: FormGroup;
+	currentAssignment: Assignment;
 	
 	constructor(private formBuilder: FormBuilder, public navCtrl: NavController, public navParams: NavParams, private waypointService: WaypointService) {
 		this.waypointForm = this.formBuilder.group({
-			activity: [''],
+			activity: ['', Validators.required],
 			location_attributes : this.formBuilder.group({
 				name: ['', Validators.required],
 				address: ['']
@@ -31,28 +32,42 @@ export class WaypointFormPage implements OnInit {
 			price_per_litre: [''],
 			litres_diesel: ['']
 		});
+
+		// conditionally require values if fuelling selected
+		this.waypointForm.controls.activity.valueChanges.subscribe(
+			(activity: string) => {
+				if(activity != "fuel") {
+					this.waypointForm.controls.price_per_litre.setValidators(null);
+					this.waypointForm.controls.litres_diesel.setValidators(null);
+				}
+				else {
+					this.waypointForm.controls.price_per_litre.setValidators(Validators.required);
+					this.waypointForm.controls.litres_diesel.setValidators(Validators.required);
+				}
+			}
+		);
 	}
 
 	ngOnInit(): void {
 		this.waypoint = this.navParams.get("waypoint");
-
+		this.currentAssignment = this.navParams.get("currentAssignment");
 		// if no waypoint passed then we are creating unscheduled
 		// point - set up default values
-		if(this.waypoint == null)
+		if(this.waypoint == null && this.currentAssignment)
 		{
-			let currentAssignment: Assignment = this.navParams.get("currentAssignment");
 			this.waypoint = new Waypoint();
 		 	this.waypoint.scheduled = false;
-			this.waypoint.route_id = currentAssignment.route.id;
 			this.waypoint.reading_attributes = new Reading();
-			this.waypoint.reading_attributes.truck_id = currentAssignment.truck_id;
 			this.waypoint.scheduled_date = new Date();
 			this.waypoint.actual_date = this.waypoint.scheduled_date;
 		}
 	}
 
 	submit(): void {
-		this.waypoint = this.waypointForm.value;
-		this.waypointService.create(this.waypoint).subscribe(() => this.navCtrl.pop());
+		// typescript merge dictionaries
+		let waypoint = { ...this.waypoint, ...this.waypointForm.value };
+		waypoint.reading_attributes.truck_id = this.currentAssignment.truck_id;
+		waypoint.route_id = this.currentAssignment.route.id;
+		this.waypointService.create(waypoint).subscribe(() => this.navCtrl.pop());
 	}
 }
